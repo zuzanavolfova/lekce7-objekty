36b41c8e78ac283b6aebe9ec88e0794a17dcb1a8
karolyim <matej.karolyi@gmail.com>
2022-03-29 19:16:50 +0200

    [FEATURE] first animal added to the farm

--- 02-farma-zvirat/main.js
+++ 02-farma-zvirat/main.js
@@ -1,10 +1,10 @@
 let krava = {
-    jmeno: 'Kravicka',
+    jmeno: 'Kravička',
     foto: 'obrazky/krava.jpg'
 };
 
 let ovecka = {
-    jmeno: 'Ovecka',
+    jmeno: 'Ovečka',
     foto: 'obrazky/ovce.jpg'
 };
 
@@ -13,25 +13,36 @@ let ovecka = {
 // 1. Zacneme vytvorenim obalujiciho div element s tridou 'zvire'.
 
     //let zvire =
+let zvire = document.createElement('div');
+zvire.className = 'zvire';
 
 
 // 2. Nyni vytvor dva elementy pro obrazek a jmeno zvirete, ktere budou uvnitr naseho obalujiciho div elementu zvire.
     //2.1. Vytvor img element s tridou 'foto'. Jako src obrazku pridej hodnotu objektu krava.
 
     //let fotoZvirete =
+    let fotoZvirete = document.createElement('img');
+    fotoZvirete.className = 'foto';
+    fotoZvirete.src = krava.foto;
 
     //2.2. Dale vytvor div element s tridou 'jmeno'. Jako text elementu zobraz hodnotu vlastnosti jmeno objektu krava.
 
     //let jmenoZvirete =
+    let jmenoZvirete = document.createElement('span');
+    jmenoZvirete.className = 'jmeno';
+    jmenoZvirete.innerHTML = krava.jmeno;
 
     //2.3. Zabal fotoZvire a jmenoZvirete do obalovaciho divu zvire, (psst zvire.appendChild)
+    zvire.appendChild(fotoZvirete);
+    zvire.appendChild(jmenoZvirete);
 
 
 // 3. Nakonec pojdme nas kod zapojit do html stranky. Na html strance vyber existujici element s tridou farma.
 // a vloz do nej obalujici element zvire (pssst appendChild)
 
   //let farma =
-
+let farma = document.querySelector('.farma');
+farma.appendChild(zvire);
 
 // Bonus
 // Co kdybychom meli program, kde tech zvirat je vic - objekt krava, objekt ovce, objekt prase...
