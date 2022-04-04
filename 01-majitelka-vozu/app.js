57a62d410781ac88da5a410ea65dfb63050422c4
Zuzana Volfova <zuzka.volfova@gmail.com>
2022-04-04 08:44:46 +0200

    done ukol

--- 01-majitelka-vozu/app.js
+++ 01-majitelka-vozu/app.js
@@ -1,10 +1,20 @@
 // Vytvoř objekt auto, který bude mít vlastnosti: znacka, barva, rokVyroby,..
 
+let auto = {
+    znacka: 'ford',
+    barva: 'modra',
+    rokVzroby: '2000'
+}
 
 // Přidej novou vlastnost (majitel a prirad ji svoje jmeno jako hodnotu)
 
+auto.majitel = 'Zuzka'
 
 // Přepiš značku na Škoda
-
+auto.znacka = 'skoda'
 
 // Vytvor tri promenne majitelka, znacka a spz a vloz do techto elementu text z objektu auto.
+let majitelka = auto.majitel
+let znacka = auto.znacka
+auto.spz = '4z1 1234'
+let spz = auto.spz
