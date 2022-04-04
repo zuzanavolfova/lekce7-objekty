ac6d358964347fc43e8d97e9843a6c2c14b8494d
Zuzana Volfova <zuzka.volfova@gmail.com>
2022-04-04 09:22:01 +0200

    first animal edit

--- 02-farma-zvirat/main.js
+++ 02-farma-zvirat/main.js
@@ -12,25 +12,34 @@ let ovecka = {
 
 // 1. Zacneme vytvorenim obalujiciho div element s tridou 'zvire'.
 
-    //let zvire =
+    let zvire = document.createElement ('div');
+    zvire.className = 'zvire';
 
 
 // 2. Nyni vytvor dva elementy pro obrazek a jmeno zvirete, ktere budou uvnitr naseho obalujiciho div elementu zvire.
     //2.1. Vytvor img element s tridou 'foto'. Jako src obrazku pridej hodnotu objektu krava.
 
-    //let fotoZvirete =
+    let fotoZvirete = document.createElement ('img');
+    fotoZvirete.className = 'foto';
+    fotoZvirete.src = krava.foto;
+
 
     //2.2. Dale vytvor div element s tridou 'jmeno'. Jako text elementu zobraz hodnotu vlastnosti jmeno objektu krava.
+    let jmenoZvirete = document.createElement ('span');
+    jmenoZvirete.innerHTML = krava.jmeno;
+    jmenoZvirete.className = 'jmeno';
 
-    //let jmenoZvirete =
 
     //2.3. Zabal fotoZvire a jmenoZvirete do obalovaciho divu zvire, (psst zvire.appendChild)
+zvire.appendChild(fotoZvirete);
+zvire.appendChild(jmenoZvirete);
 
 
 // 3. Nakonec pojdme nas kod zapojit do html stranky. Na html strance vyber existujici element s tridou farma.
 // a vloz do nej obalujici element zvire (pssst appendChild)
 
-  //let farma =
+  let farma = document.querySelector('.farma');
+  farma.appendChild(zvire);
 
 
 // Bonus
