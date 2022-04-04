1c07de434de1b71dc09273a1003e2e88c53f817c
Zuzana Volfova <zuzka.volfova@gmail.com>
2022-04-04 09:53:13 +0200

    done bonus

--- 02-farma-zvirat/main.js
+++ 02-farma-zvirat/main.js
@@ -1,13 +1,30 @@
-let krava = {
-    jmeno: 'Kravicka',
-    foto: 'obrazky/krava.jpg'
-};
 
-let ovecka = {
-    jmeno: 'Ovecka',
-    foto: 'obrazky/ovce.jpg'
-};
+const seznamZvirat = [
+    {
+        jmeno: 'Husa',
+        foto: 'obrazky/husa.jpg'
+    },
+    {
+        jmeno: 'Kocka',
+        foto: 'obrazky/kocka.jpg'
+    },
+    {
+        jmeno: 'Kun',
+        foto: 'obrazky/kun.jpg'
+    },
+    {
+        jmeno: 'Ovce',
+        foto: 'obrazky/Ovce.jpg'
+    },
+    {
+        jmeno: 'Pes',
+        foto: 'obrazky/pes.jpg'
+    }
+];
 
+
+seznamZvirat.forEach(vypisZvirat);
+function vypisZvirat (seznamZvirat) {
 // Vytvor obrazek zviratka krava s fotkou a popiskem.
 
 // 1. Zacneme vytvorenim obalujiciho div element s tridou 'zvire'.
@@ -21,12 +38,13 @@ let ovecka = {
 
     let fotoZvirete = document.createElement ('img');
     fotoZvirete.className = 'foto';
-    fotoZvirete.src = krava.foto;
+    fotoZvirete.src = seznamZvirat.foto;
+    fotoZvirete.alt = seznamZvirat.jmeno;
 
 
     //2.2. Dale vytvor div element s tridou 'jmeno'. Jako text elementu zobraz hodnotu vlastnosti jmeno objektu krava.
     let jmenoZvirete = document.createElement ('span');
-    jmenoZvirete.innerHTML = krava.jmeno;
+    jmenoZvirete.innerHTML = seznamZvirat.jmeno;
     jmenoZvirete.className = 'jmeno';
 
 
@@ -40,8 +58,26 @@ zvire.appendChild(jmenoZvirete);
 
   let farma = document.querySelector('.farma');
   farma.appendChild(zvire);
+}
 
 
-// Bonus
-// Co kdybychom meli program, kde tech zvirat je vic - objekt krava, objekt ovce, objekt prase...
-// Musela bych pro kazdy objekt mit samostatnou promennou...jde to lépe?
+// for (let i =0; i < seznamZvirat.length; i++) {
+// let zvire = document.createElement ('div');
+// zvire.className = 'zvire';
+
+// let fotoZvirete = document.createElement ('img');
+// fotoZvirete.className = 'foto';
+// fotoZvirete.src = seznamZvirat[i].foto;
+// fotoZvirete.alt =seznamZvirat[i].jmeno;
+
+// let jmenoZvirete = document.createElement ('span');
+// jmenoZvirete.innerHTML = seznamZvirat[i].jmeno;
+// jmenoZvirete.className = 'jmeno';
+
+
+// zvire.appendChild(fotoZvirete);
+// zvire.appendChild(jmenoZvirete);
+
+// let farma = document.querySelector('.farma');
+// farma.appendChild(zvire);
+// }
